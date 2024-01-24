d500176ca00a63ef01bef8bea69b01b27ccaf5d7
mike-kotikov <37017407+mike-kotikov@users.noreply.github.com>
2024-01-24 23:23:33 +0300

    Adds implementations for cli functions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
+import { argv } from 'node:process';
+
+const ARG_PREFIX = '--';
+
 const parseArgs = () => {
-    // Write your code here 
+  const resultArr = [];
+
+  for (let i = 0; i < argv.length; ++i) {
+    if (!argv[i].includes(ARG_PREFIX)) {
+      continue;
+    }
+    resultArr.push(`${argv[i].slice(2)} is ${argv[i + 1]}`);
+  }
+
+  console.log(resultArr.join(', '));
 };
 
 parseArgs();
