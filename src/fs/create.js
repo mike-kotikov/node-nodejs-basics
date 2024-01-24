32d81f21beec844d97f7c3f271bbbeec8446dce9
mike-kotikov <37017407+mike-kotikov@users.noreply.github.com>
2024-01-24 16:24:34 +0300

    Adds implementations for fs functions

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,29 @@
+import { fileURLToPath } from 'node:url';
+import { writeFile, access } from 'node:fs/promises';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
+const filePath = `${__dirname}files/fresh.txt`;
+
+const checkFileExists = async path => {
+  try {
+    await access(path);
+
+    return true;
+  } catch (err) {
+    return false;
+  }
+};
+
 const create = async () => {
-    // Write your code here 
+  const fileExists = await checkFileExists(filePath);
+
+  if (fileExists) {
+    throw new Error('FS operation failed');
+  }
+
+  // Create file with contents
+  await writeFile(filePath, 'I am fresh and young');
 };
 
 await create();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,17 @@
+import { fileURLToPath } from 'node:url';
+import { rename as fsRename } from 'node:fs/promises';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
+const fileToRenamePath = `${__dirname}files/wrongFilename.txt`;
+const newFilePath = `${__dirname}files/properFilename.md`;
+
 const rename = async () => {
-    // Write your code here 
+  try {
+    await fsRename(fileToRenamePath, newFilePath);
+  } catch (err) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await rename();
