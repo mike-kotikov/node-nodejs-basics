068b9a646745770fb74fa3d6f7642cc160ee028b
mike-kotikov <37017407+mike-kotikov@users.noreply.github.com>
2024-01-27 22:26:12 +0300

    Adds implementation for hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import { fileURLToPath } from 'node:url';
+import { createHash } from 'node:crypto';
+import { createReadStream } from 'node:fs';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
+const filePath = `${__dirname}files/fileToCalculateHashFor.txt`;
+
+const fileHash = createHash('sha256');
+const fileReadStream = createReadStream(filePath)
+
 const calculateHash = async () => {
-    // Write your code here 
+    fileReadStream.on('readable', () => {
+        const content = fileReadStream.read();
+
+        if (content) {
+            fileHash.update(content);
+        } else {
+            console.log(fileHash.digest('hex'));
+        }
+    })
 };
 
 await calculateHash();
