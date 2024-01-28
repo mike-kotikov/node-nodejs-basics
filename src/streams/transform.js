6aafe6fa49c2709492d932eadc53c31c245a4fd4
mike-kotikov <37017407+mike-kotikov@users.noreply.github.com>
2024-01-28 15:34:13 +0300

    Adds implementations for streams functions

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,16 @@
+import { stdin, stdout } from 'node:process';
+import { pipeline } from 'node:stream/promises';
+
+const transformStream = new TransformStream({
+  transform: (data, controller) => {
+    const strData = data.toString().trim();
+    const reversed = strData.split('').reverse().join('');
+    controller.enqueue(`${reversed}\n`);
+  }
+});
+
 const transform = async () => {
-    // Write your code here 
+  await pipeline(stdin, transformStream, stdout);
 };
 
 await transform();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import { fileURLToPath } from 'node:url';
+import { stdin } from 'node:process';
+import { createWriteStream } from 'node:fs';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
+const fileToWritePath = `${__dirname}files/fileToWrite.txt`;
+
+const writeStream = createWriteStream(fileToWritePath, { encoding: 'utf8' });
+
 const write = async () => {
-    // Write your code here 
+  stdin.pipe(writeStream);
 };
 
 await write();
