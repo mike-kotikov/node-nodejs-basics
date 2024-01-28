24a8cc46d5ef91a06424f700e92a707243f2fa88
mike-kotikov <37017407+mike-kotikov@users.noreply.github.com>
2024-01-28 15:57:43 +0300

    Adds implementations for zip functions

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,23 @@
+import { fileURLToPath } from 'node:url';
+import { pipeline } from 'node:stream/promises';
+import { createGzip } from 'node:zlib';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { rm } from 'node:fs/promises';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
+const fileToCompressPath = `${__dirname}files/fileToCompress.txt`;
+const outputArchivePath = `${__dirname}files/archive.gz`;
+
+const fileReadStream = createReadStream(fileToCompressPath);
+const fileWriteStream = createWriteStream(outputArchivePath);
+
+const gzip = createGzip();
+
 const compress = async () => {
-    // Write your code here 
+  await pipeline(fileReadStream, gzip, fileWriteStream);
+  // Remove file after it's been zipped
+  await rm(fileToCompressPath);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,23 @@
+import { fileURLToPath } from 'node:url';
+import { pipeline } from 'node:stream/promises';
+import { createUnzip } from 'node:zlib';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { rm } from 'node:fs/promises';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
+const archivePath = `${__dirname}files/archive.gz`;
+const outputFilePath = `${__dirname}files/fileToCompress.txt`;
+
+const archiveReadStream = createReadStream(archivePath);
+const fileWriteStream = createWriteStream(outputFilePath);
+
+const unzip = createUnzip();
+
 const decompress = async () => {
-    // Write your code here 
+  await pipeline(archiveReadStream, unzip, fileWriteStream);
+  // Remove archive after file unzip
+  await rm(archivePath);
 };
 
 await decompress();
