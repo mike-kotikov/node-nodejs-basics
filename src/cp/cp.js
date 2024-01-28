c9ff39c12f7cd62b23d926112553060c6609673b
mike-kotikov <37017407+mike-kotikov@users.noreply.github.com>
2024-01-28 19:05:30 +0300

    Adds implementation for child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,13 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import { fileURLToPath } from 'node:url';
+import { fork } from 'node:child_process';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
+const scriptPath = `${__dirname}files/script.js`;
+
+const spawnChildProcess = async args => {
+  fork(scriptPath, args);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([123, 'test string', true]);
